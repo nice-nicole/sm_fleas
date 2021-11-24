3846dcc8d8e85b2fde7355f50f1007ab8773abc5
nice-nicole <nicenicole.in@gmail.com>
2021-11-24 16:53:23 +0200

    trying to do part 2

--- flea.py
+++ flea.py
@@ -5,15 +5,18 @@ a = 20
 b = 0
 count = 0
 for i in range(1, n+1):
-    print("-------------------------------")
-    print("At time = ", i)
-    random_number = random.random()
-    print("generated random number = ", random_number)
-    print("a/n = ", a/n)
-    if(random_number <= a/n):
-        a -= 1
-        b += 1
-    else:
-        a += 1
-        b -= 1
-    print(a, b)
+    while(a>0):
+        count+=1
+        print("-------------------------------")
+        print("At time = ", i)
+        random_number = random.random()
+        print("generated random number = ", random_number)
+        print("a/n = ", a/n)
+        if(random_number <= a/n):
+            a -= 1
+            b += 1
+        else:
+            a += 1
+            b -= 1
+        print(a, b)
+        print(count)
