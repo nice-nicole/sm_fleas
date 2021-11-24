4cc78017197c5fc7f8b590c223ccd5c06df1c500
nice-nicole <nicenicole.in@gmail.com>
2021-11-24 13:51:05 +0200

    project 3 about fleas simulation and modelling

---
+++ flea.py
@@ -0,0 +1,20 @@
+import random
+n = 20
+# initialize a and b at time 0
+a = 20
+b = 0
+for i in range (1, n-1):
+    print("-------------------------------")
+    print("At time = ", i)
+    random_number= random.random()
+    print("generated random number = ", random_number)
+    print("a/n = ", a/n)
+    if(random_number <= a/n):
+        a-=1
+        b+=1
+    else:
+        a+=1
+        b-=1
+    print(a,b)
+
+
