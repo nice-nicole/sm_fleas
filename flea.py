8abd7def0e6fc38a44c4d762a0142ab9e262f2fa
nice-nicole <nicenicole.in@gmail.com>
2021-11-24 16:31:25 +0200

    part one

--- flea.py
+++ flea.py
@@ -3,18 +3,17 @@ n = 20
 # initialize a and b at time 0
 a = 20
 b = 0
-for i in range (1, n+1):
+count = 0
+for i in range(1, n+1):
     print("-------------------------------")
     print("At time = ", i)
-    random_number= random.random()
+    random_number = random.random()
     print("generated random number = ", random_number)
     print("a/n = ", a/n)
     if(random_number <= a/n):
-        a-=1
-        b+=1
+        a -= 1
+        b += 1
     else:
-        a+=1
-        b-=1
-    print(a,b)
-
-
+        a += 1
+        b -= 1
+    print(a, b)
