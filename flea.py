62baaa1fe279fa7c43b5abcf6abb93618c1da9f2
nice-nicole <nicenicole.in@gmail.com>
2021-11-29 11:38:28 +0200

    final changes

--- flea.py
+++ flea.py
@@ -1,11 +1,12 @@
 import random
 n = 20
-# initialize a and b at time 0
-a = 20
-b = 0
-count = 1
+
+sum =0
 for i in range(1, n+1):
-    while(a>0):
+    a = 19
+    b = 1
+    count = 1
+    while(a!=20):
         count +=1
         # print("-------------------------------")
         # print("At time = ", i)
@@ -18,8 +19,11 @@ for i in range(1, n+1):
         else:
             a += 1
             b -= 1
-        if(a==20 & b==0):
-            break
         # print("(" ,a ,",", b,")")
-print(i, "iterations")        
-print("average value : ", count/20)
+    print(i, "iterations")        
+    print("count : ", count)
+
+    sum += count
+print("average value :" ,sum/20)
+
+
